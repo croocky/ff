4e1916ab65bcfcb05c7f68b3142c67f8624dc75b
socaseinpoint <socaseinpoint@gmail.com>
2017-10-07 12:11:48 +0400

    add template

---
+++ blog/resources/views/introduction.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Swifty</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+  <link rel="stylesheet" href="css/main.css">
+
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+
+</head>
+<body>
+
+  <div class="content content_guest">
+    //= templates/header.html
+
+    <div class="content__inner">
+
+      <ul class="cb-slideshow">
+          <li><span>Image 01</span><div><h3>тех·но·логии</h3></div></li>
+          <li><span>Image 02</span><div><h3>com·po·sure</h3></div></li>
+          <li><span>Image 03</span><div><h3>e·qua·nim·i·ty</h3></div></li>
+          <li><span>Image 04</span><div><h3>bal·ance</h3></div></li>
+          <li><span>Image 05</span><div><h3>qui·e·tude</h3></div></li>
+          <li><span>Image 06</span><div><h3>re·lax·a·tion</h3></div></li>
+      </ul>
+
+      <div class="superstar">
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+      </div>
+
+      <!--
+      <div class="sector bg_hot">
+        <div class="heading">SWIFTY — ВЫСОКОТЕХНОЛОГИЧНЫЙ ПОДХОД К ИНВЕСТИЦИЯМ</div>
+        <img src="imgsvg/heading.svg" class="img_full-page" alt="heading">
+        <img src="imgsvg/mousedown.svg" alt="" class="mousedown">
+
+        <div id="large-header" class="large-header">
+        <canvas id="demo-canvas"></canvas>
+        </div>
+      </div>
+    -->
+      <div class="sector bg_black">
+        <div class="heading text_white">ADVANTAGES</div>
+        <div class="row text-center">
+          <div class="four columns">
+            <div class="subheading"> НЕВЕРОЯТНЫИ РОСТ</div>
+            <img src="imgsvg/i-increase.svg" alt="Increase" class="i-big">
+            <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
+          </div>
+          <div class="four columns">
+            <div class="subheading"> ЗАМОК WI-FI</div>
+            <img src="imgsvg/i-secure.svg" alt="Increase" class="i-big">
+            <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
+          </div>
+          <div class="four columns">
+            <div class="subheading">ПЛАНЕТА ЗЕМЛЯ</div>
+            <img src="imgsvg/i-world.svg" alt="Increase" class="i-big">
+            <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+      <div class="sector">
+          <div class="heading">FOR TRAIDERS</div>
+          <img src="imgsvg/world.svg" class="img_full-page" alt="heading">
+          <br>
+          <div class="row text-center">
+            <div class="three columns">
+              <img src="imgsvg/germany.svg" alt="Germany" class="i-flag">
+              <div class="subheading subheading_thin">Германия</div>
+            </div>
+            <div class="three columns">
+              <img src="imgsvg/belgium.svg" alt="Belgium" class="i-flag">
+              <div class="subheading subheading_thin">Бельгия</div>
+            </div>
+            <div class="three columns">
+              <img src="imgsvg/czech-republic.svg" alt="Belgium" class="i-flag">
+              <div class="subheading subheading_thin">Чехия</div>
+            </div>
+            <div class="three columns">
+              <img src="imgsvg/denmark.svg" alt="Denmark" class="i-flag">
+              <div class="subheading subheading_thin">Дания</div>
+            </div>
+          </div>
+      </div>
+
+      <div class="sector bg_black">
+        <div class="heading">FAQ</div>
+        <div class="row">
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+          <div class="six columns">
+            <div class="subheading-small">1. Question</div>
+            <div class="text">Summer is almost here and what better way to motivate your employees than with an abundance of brand name gifts that are perfect for summer activities! Whether you already have an employee incentive</div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+        //= templates/footer.html
+
+  </div>
+
+  <div class="login rightsidebar">
+    <div class="login__heading-nav">
+      <div class="login__heading-nav-item login__heading-nav-item_active">
+        Войти
+      </div>
+      <div class="login__heading-nav-item ">
+        Регистрация
+      </div>
+    </div>
+    <div class="login__content">
+      <input type="text">
+      <input type="text">
+      <input type="submit" value="Войти">
+
+      <div class="login__lable">
+        <div class="login__lable-item">
+          <img class="login__lable-item-img" src="imgsvg/cap.svg" alt="Cap">
+          <span class="login__lable-item-text">Ваши инвестиции застрахованны</span>
+        </div>
+        <div class="login__lable-item">
+          <img class="login__lable-item-img" src="imgsvg/visa.svg" alt="Visa">
+          <span class="login__lable-item-text">Выплаты на международные платежные  системы</span>
+        </div>
+        <div class="login__lable-item">
+          <img class="login__lable-item-img" src="imgsvg/key.svg" alt="Key">
+          <span class="login__lable-item-text">Ваши персональные данные под надежной защитой</span>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+      //= templates/popups.html
+
+
+
+      <script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/EasePack.min.js'></script>
+      <script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/rAF.js'></script>
+      <script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/TweenLite.min.js'></script>
+      <script src="js/main.js"></script>
+
+      <script>
+        window.onscroll = function() {
+          if($(window).scrollTop()>$('.cb-slideshow').height()){
+                $('.header').addClass('header_bg');
+            } else {
+                $('.header').removeClass('header_bg');
+            }
+        }
+
+        function openlogin(){
+          $('.content').toggleClass('content_hide');
+          $('.rightsidebar').toggle();
+        }
+      </script>
+
+</body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('introduction');
 });
 Route::get('/users', 'TestController@index');
 Route::post('update', 'UpdateController@index')->name('update');
