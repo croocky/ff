1410bd297971ae0b0ede981719378799812ba36f
socaseinpoint <socaseinpoint@gmail.com>
2017-10-07 21:19:22 +0400

    images

--- blog/resources/views/layouts/introduction.blade.php
+++ blog/resources/views/layouts/introduction.blade.php
@@ -32,8 +32,7 @@
 <body>
 
   <div class="content content_guest">
-    @extends('layouts.app')
-     @yield('header')
+
 <div class="chart">
 </div>
 
@@ -58,8 +57,8 @@
       <!--
       <div class="sector bg_hot">
         <div class="heading">SWIFTY — ВЫСОКОТЕХНОЛОГИЧНЫЙ ПОДХОД К ИНВЕСТИЦИЯМ</div>
-        <img src="imgsvg/heading.svg" class="img_full-page" alt="heading">
-        <img src="imgsvg/mousedown.svg" alt="" class="mousedown">
+        <img src="{!! asset('images/imgsvg/heading.svg" class="img_full-page" alt="heading">
+        <img src="{!! asset('images/imgsvg/mousedown.svg" alt="" class="mousedown">
 
         <div id="large-header" class="large-header">
         <canvas id="demo-canvas"></canvas>
@@ -71,17 +70,17 @@
         <div class="row text-center">
           <div class="four columns">
             <div class="subheading"> НЕВЕРОЯТНЫИ РОСТ</div>
-            <img src="imgsvg/i-increase.svg" alt="Increase" class="i-big">
+            <img src="{!! asset('images/imgsvg/i-increase.svg" alt="Increase" class="i-big">
             <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
           </div>
           <div class="four columns">
             <div class="subheading"> ЗАМОК WI-FI</div>
-            <img src="imgsvg/i-secure.svg" alt="Increase" class="i-big">
+            <img src="{!! asset('images/imgsvg/i-secure.svg" alt="Increase" class="i-big">
             <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
           </div>
           <div class="four columns">
             <div class="subheading">ПЛАНЕТА ЗЕМЛЯ</div>
-            <img src="imgsvg/i-world.svg" alt="Increase" class="i-big">
+            <img src="{!! asset('images/imgsvg/i-world.svg" alt="Increase" class="i-big">
             <div class="text text_small-width">Тут быквы чисто для примера, ну думаю хватит, еще чутка, и еще</div>
           </div>
         </div>
@@ -92,23 +91,23 @@
 
       <div class="sector">
           <div class="heading">FOR TRAIDERS</div>
-          <img src="imgsvg/world.svg" class="img_full-page" alt="heading">
+          <img src="{!! asset('images/imgsvg/world.svg" class="img_full-page" alt="heading">
           <br>
           <div class="row text-center">
             <div class="three columns">
-              <img src="imgsvg/germany.svg" alt="Germany" class="i-flag">
+              <img src="{!! asset('images/imgsvg/germany.svg" alt="Germany" class="i-flag">
               <div class="subheading subheading_thin">Германия</div>
             </div>
             <div class="three columns">
-              <img src="imgsvg/belgium.svg" alt="Belgium" class="i-flag">
+              <img src="{!! asset('images/imgsvg/belgium.svg" alt="Belgium" class="i-flag">
               <div class="subheading subheading_thin">Бельгия</div>
             </div>
             <div class="three columns">
-              <img src="imgsvg/czech-republic.svg" alt="Belgium" class="i-flag">
+              <img src="{!! asset('images/imgsvg/czech-republic.svg" alt="Belgium" class="i-flag">
               <div class="subheading subheading_thin">Чехия</div>
             </div>
             <div class="three columns">
-              <img src="imgsvg/denmark.svg" alt="Denmark" class="i-flag">
+              <img src="{!! asset('images/imgsvg/denmark.svg" alt="Denmark" class="i-flag">
               <div class="subheading subheading_thin">Дания</div>
             </div>
           </div>
