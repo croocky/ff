bb3cc71b63ce8c14d6836567c1d9aeecd6aabe5f
socaseinpoint <socaseinpoint@gmail.com>
2017-10-07 14:20:24 +0400

    jquery

--- blog/resources/views/introduction.blade.php
+++ blog/resources/views/introduction.blade.php
@@ -222,8 +222,8 @@
       <script src="{!! asset('js/all.js') !!}"></script>
 
       <script>
-      var $ = require('jquery');
-      
+      window.$ = window.jQuery = require('jquery');
+
         window.onscroll = function() {
           if($(window).scrollTop()>$('.cb-slideshow').height()){
                 $('.header').addClass('header_bg');
