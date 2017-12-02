a6681040a2f9b4d8d414bf489226f9612c76ba0a
socaseinpoint <socaseinpoint@gmail.com>
2017-12-02 07:57:27 +0400

    template

--- blog/public/js/app.js
+++ blog/public/js/app.js
@@ -11060,230 +11060,12 @@ window.Vue = __webpack_require__(35);
 Vue.component('example-component', __webpack_require__(36));
 
 var app = new Vue({
-    el: '#app'
+  el: '#app'
 });
 
 global.$ = global.jQuery = __webpack_require__(3);
-(function () {
 
-    var width,
-        height,
-        largeHeader,
-        canvas,
-        ctx,
-        points,
-        target,
-        animateHeader = true;
-
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
-
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = { x: width / 2, y: height / 2 };
-
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height + 'px';
-
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
-
-        // create points
-        points = [];
-        for (var x = 0; x < width; x = x + width / 20) {
-            for (var y = 0; y < height; y = y + height / 20) {
-                var px = x + Math.random() * width / 20;
-                var py = y + Math.random() * height / 20;
-                var p = { x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
-        }
-
-        // for each point find the 5 closest points
-        for (var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for (var j = 0; j < points.length; j++) {
-                var p2 = points[j];
-                if (!(p1 == p2)) {
-                    var placed = false;
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
-
-        // assign a circle to each point
-        for (var i in points) {
-            var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
-    }
-
-    // Event handling
-    function addListeners() {
-        if (!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        } else if (e.clientX || e.clientY) {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
-
-    function scrollCheck() {
-        if (document.body.scrollTop > height) animateHeader = false;else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = height + 'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    // animation
-    function initAnimation() {
-        animate();
-        for (var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if (animateHeader) {
-            ctx.clearRect(0, 0, width, height);
-            for (var i in points) {
-                // detect points in range
-                if (Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1 + 1 * Math.random(), { x: p.originX - 50 + Math.random() * 100,
-            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
-            onComplete: function onComplete() {
-                shiftPoint(p);
-            } });
-    }
-
-    // Canvas manipulation
-    function drawLines(p) {
-        if (!p.active) return;
-        for (var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos, rad, color) {
-        var _this = this;
-
-        // constructor
-        (function () {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function () {
-            if (!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
-            ctx.fill();
-        };
-    }
-
-    // Util
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-})();
-
-$.ajax({
-    url: "https://api.coindesk.com/v1/bpi/currentprice.json"
-}).done(function (data) {
-    var jsondata = JSON.parse(data);
-    var pound = jsondata.bpi.GBP.symbol + jsondata.bpi.GBP.rate_float;
-    var euro = jsondata.bpi.EUR.symbol + jsondata.bpi.EUR.rate_float;
-    var dollar = jsondata.bpi.USD.symbol + jsondata.bpi.USD.rate_float;
-    $('.chart').append('<i class="chart__item chart__item_lable">Bitcoin Price Index (BPI)</i><i class="chart__item">' + pound + '</i><i class="chart__item">' + euro + '</i><i class="chart__item">' + dollar + '</i>');
-});
-
-$(function () {
-
-    var marquee = $(".chart");
-    marquee.css({ "overflow": "hidden", "width": "100%" });
-
-    // оболочка для текста ввиде span (IE не любит дивы с inline-block)
-    marquee.wrapInner("<span>");
-    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align": "center" });
-    marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
-
-    marquee.wrapInner("<div>");
-    marquee.find("div").css("width", "200%");
-
-    var reset = function reset() {
-        $(this).css("margin-left", "0%");
-        $(this).animate({ "margin-left": "-100%" }, 12000, 'linear', reset);
-    };
-
-    reset.call(marquee.find("div"));
-});
+__webpack_require__(50);
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
 /***/ }),
@@ -42330,7 +42112,242 @@ if (false) {
 /* 40 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleNotFoundError: Module not found: Error: Can't resolve '../../img/1.jpg' in 'd:\\PRJCT\\new\\swiftygit\\ff\\blog\\resources\\assets\\sass'\n    at factoryCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\Compilation.js:269:40)\n    at factory (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\NormalModuleFactory.js:235:20)\n    at resolver (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\NormalModuleFactory.js:60:20)\n    at asyncLib.parallel.e (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\NormalModuleFactory.js:127:20)\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\async\\dist\\async.js:3861:9\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\async\\dist\\async.js:421:16\n    at iteratorCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\async\\dist\\async.js:996:13)\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\async\\dist\\async.js:906:16\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\async\\dist\\async.js:3858:13\n    at resolvers.normal.resolve (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\NormalModuleFactory.js:119:22)\n    at onError (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:65:10)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at runAfter (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:158:4)\n    at innerCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:146:3)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\tapable\\lib\\Tapable.js:252:11)\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\UnsafeCachePlugin.js:40:4\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at runAfter (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:158:4)\n    at innerCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:146:3)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\tapable\\lib\\Tapable.js:252:11)\n    at innerCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:144:11)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\tapable\\lib\\Tapable.js:249:35)\n    at resolver.doResolve.createInnerCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:44:6)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at afterInnerCallback (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\Resolver.js:168:10)\n    at loggingCallbackWrapper (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\tapable\\lib\\Tapable.js:252:11)");
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open 'd:\\PRJCT\\new\\swiftygit\\ff\\blog\\resources\\assets\\sass\\res\\font-awesome.min.css'\n    at Error (native)\n    at runLoaders (d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\loader-runner\\lib\\LoaderRunner.js:200:19\n    at d:\\PRJCT\\new\\swiftygit\\ff\\blog\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:70:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)");
+
+/***/ }),
+/* 41 */,
+/* 42 */,
+/* 43 */,
+/* 44 */,
+/* 45 */,
+/* 46 */,
+/* 47 */,
+/* 48 */,
+/* 49 */,
+/* 50 */
+/***/ (function(module, exports) {
+
+
+(function () {
+
+    var width,
+        height,
+        largeHeader,
+        canvas,
+        ctx,
+        points,
+        target,
+        animateHeader = true;
+
+    // Main
+    initHeader();
+    initAnimation();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = { x: width / 2, y: height / 2 };
+
+        largeHeader = document.getElementById('large-header');
+        largeHeader.style.height = height + 'px';
+
+        canvas = document.getElementById('demo-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create points
+        points = [];
+        for (var x = 0; x < width; x = x + width / 20) {
+            for (var y = 0; y < height; y = y + height / 20) {
+                var px = x + Math.random() * width / 20;
+                var py = y + Math.random() * height / 20;
+                var p = { x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
+
+        // for each point find the 5 closest points
+        for (var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for (var j = 0; j < points.length; j++) {
+                var p2 = points[j];
+                if (!(p1 == p2)) {
+                    var placed = false;
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        // assign a circle to each point
+        for (var i in points) {
+            var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
+        }
+    }
+
+    // Event handling
+    function addListeners() {
+        if (!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        } else if (e.clientX || e.clientY) {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
+
+    function scrollCheck() {
+        if (document.body.scrollTop > height) animateHeader = false;else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height + 'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    // animation
+    function initAnimation() {
+        animate();
+        for (var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if (animateHeader) {
+            ctx.clearRect(0, 0, width, height);
+            for (var i in points) {
+                // detect points in range
+                if (Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1 + 1 * Math.random(), { x: p.originX - 50 + Math.random() * 100,
+            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
+            onComplete: function onComplete() {
+                shiftPoint(p);
+            } });
+    }
+
+    // Canvas manipulation
+    function drawLines(p) {
+        if (!p.active) return;
+        for (var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos, rad, color) {
+        var _this = this;
+
+        // constructor
+        (function () {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function () {
+            if (!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
+            ctx.fill();
+        };
+    }
+
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+})();
+
+$.ajax({
+    url: "https://api.coindesk.com/v1/bpi/currentprice.json"
+}).done(function (data) {
+    var jsondata = JSON.parse(data);
+    var pound = jsondata.bpi.GBP.symbol + jsondata.bpi.GBP.rate_float;
+    var euro = jsondata.bpi.EUR.symbol + jsondata.bpi.EUR.rate_float;
+    var dollar = jsondata.bpi.USD.symbol + jsondata.bpi.USD.rate_float;
+    $('.chart').append('<i class="chart__item chart__item_lable">Bitcoin Price Index (BPI)</i><i class="chart__item">' + pound + '</i><i class="chart__item">' + euro + '</i><i class="chart__item">' + dollar + '</i>');
+});
+
+$(function () {
+
+    var marquee = $(".chart");
+    marquee.css({ "overflow": "hidden", "width": "100%" });
+
+    // оболочка для текста ввиде span (IE не любит дивы с inline-block)
+    marquee.wrapInner("<span>");
+    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align": "center" });
+    marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
+
+    marquee.wrapInner("<div>");
+    marquee.find("div").css("width", "200%");
+
+    var reset = function reset() {
+        $(this).css("margin-left", "0%");
+        $(this).animate({ "margin-left": "-100%" }, 12000, 'linear', reset);
+    };
+
+    reset.call(marquee.find("div"));
+});
 
 /***/ })
 /******/ ]);
---
+++ blog/resources/assets/js/design.js
@@ -0,0 +1,218 @@
+
+(function() {
+
+    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+    // Main
+    initHeader();
+    initAnimation();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: width/2, y: height/2};
+
+        largeHeader = document.getElementById('large-header');
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById('demo-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create points
+        points = [];
+        for(var x = 0; x < width; x = x + width/20) {
+            for(var y = 0; y < height; y = y + height/20) {
+                var px = x + Math.random()*width/20;
+                var py = y + Math.random()*height/20;
+                var p = {x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
+
+        // for each point find the 5 closest points
+        for(var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for(var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if(!(p1 == p2)) {
+                    var placed = false;
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        // assign a circle to each point
+        for(var i in points) {
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
+        }
+    }
+
+    // Event handling
+    function addListeners() {
+        if(!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY)    {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    // animation
+    function initAnimation() {
+        animate();
+        for(var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in points) {
+                // detect points in range
+                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }});
+    }
+
+    // Canvas manipulation
+    function drawLines(p) {
+        if(!p.active) return;
+        for(var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos,rad,color) {
+        var _this = this;
+
+        // constructor
+        (function() {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function() {
+            if(!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fill();
+        };
+    }
+
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+
+})();
+
+$.ajax({
+  url: "https://api.coindesk.com/v1/bpi/currentprice.json"
+}).done(function(data) {
+    var jsondata = JSON.parse(data);
+    var pound = jsondata.bpi.GBP.symbol+jsondata.bpi.GBP.rate_float;
+    var euro = jsondata.bpi.EUR.symbol+jsondata.bpi.EUR.rate_float;
+    var dollar = jsondata.bpi.USD.symbol+jsondata.bpi.USD.rate_float;
+    $('.chart').append('<i class="chart__item chart__item_lable">Bitcoin Price Index (BPI)</i><i class="chart__item">'+pound+'</i><i class="chart__item">'+euro+'</i><i class="chart__item">'+dollar+'</i>')
+});
+
+$(function() {
+
+    var marquee = $(".chart");
+    marquee.css({"overflow": "hidden", "width": "100%"});
+
+    // оболочка для текста ввиде span (IE не любит дивы с inline-block)
+    marquee.wrapInner("<span>");
+    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" });
+    marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
+
+    marquee.wrapInner("<div>");
+    marquee.find("div").css("width", "200%");
+
+    var reset = function() {
+        $(this).css("margin-left", "0%");
+        $(this).animate({ "margin-left": "-100%" }, 12000, 'linear', reset);
+    };
+
+    reset.call(marquee.find("div"));
+
+});
