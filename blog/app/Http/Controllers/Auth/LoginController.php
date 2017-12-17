04ebdbc9d8f4d876ee78060343ae47ed18ac0ed1
cruelka <kes1993@ya.ru>
2017-12-18 02:02:16 +0400

    htaccess

--- blog/app/Http/Controllers/Auth/LoginController.php
+++ blog/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'home', ['login' => Auth::login()];
 
     /**
      * Create a new controller instance.
