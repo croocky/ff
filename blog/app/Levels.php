70d0ee3d82f92fb62f4c7bbcc983cd165998616a
cruelka <kes1993@ya.ru>
2017-12-18 01:22:53 +0400

    htaccess

--- blog/app/Levels.php
+++ blog/app/Levels.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Levels extends Model
 {
-    public function getLevel($amount){
-        $level = where('pers_amount','<',$amount)->orderBy('pers_amount', 'desc')->take(1)->get()->toArray();
+    public static function getLevel($amount){
+        $level = $this->where('pers_amount','<',$amount)->orderBy('pers_amount', 'desc')->take(1)->get()->toArray();
         return $level;
     }
 }
