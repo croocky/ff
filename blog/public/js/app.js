2e0c2fe8b31689ce067cf5d81cca7d42d8b1e68e
socaseinpoint <socaseinpoint@gmail.com>
2017-12-18 00:51:10 +0400

    js

--- blog/public/js/app.js
+++ blog/public/js/app.js
@@ -140,9 +140,6 @@ $(document).ready(function () {
 });
 
 function openWindow(name) {
-  if ($('.windows-all').hasClass('hide')) {
-    return false;
-  }
 
   scrollTopHeight = $(window).scrollTop();
 
@@ -152,11 +149,12 @@ function openWindow(name) {
   $('.windows-overlay').addClass('windows-overlay_open');
   $('.windows').addClass('windows_open');
 
+  $('windows__item').hide();
   $(name).show();
 };
 
 function closeWindows() {
-  $('windows-item').hide();
+  $('windows__item').hide();
   $(window).scrollTop(windows);
 
   $('.windows-all').attr('style', '');
