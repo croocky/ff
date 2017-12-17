73922e5a7b19afe7980d8f11cfc03d8146e300d7
cruelka <kes1993@ya.ru>
2017-12-17 21:41:34 +0400

    htaccess

---
+++ blog/app/Http/Controllers/UserInside.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorkarpacev
+ * Date: 17.12.2017
+ * Time: 21:39
+ */
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Показать профиль данного пользователя.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return view('lc', ['user' => User::findOrFail($id)]);
+    }
+}
