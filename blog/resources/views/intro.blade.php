7dd88370e2f73e00fab650fa95197ee7c981b15d
cruelka <kes1993@ya.ru>
2017-12-18 06:27:13 +0400

    htaccess

--- blog/resources/views/intro.blade.php
+++ blog/resources/views/intro.blade.php
@@ -8,12 +8,12 @@
 
 
     <ul class="cb-slideshow">
-        <li><span>Image 01</span><div><h3>тех·но·логии</h3></div></li>
-        <li><span>Image 02</span><div><h3>com·po·sure</h3></div></li>
-        <li><span>Image 03</span><div><h3>e·qua·nim·i·ty</h3></div></li>
-        <li><span>Image 04</span><div><h3>bal·ance</h3></div></li>
-        <li><span>Image 05</span><div><h3>qui·e·tude</h3></div></li>
-        <li><span>Image 06</span><div><h3>re·lax·a·tion</h3></div></li>
+        <li><span>Image 01</span><div><h3>CRYP·TO·CUR·RENCY</h3></div></li>
+        <li><span>Image 02</span><div><h3>IN·VEST·MENTS</h3></div></li>
+        <li><span>Image 03</span><div><h3>IN·DE·PEN·DEN·CE</h3></div></li>
+        <li><span>Image 04</span><div><h3>BIT·COIN</h3></div></li>
+        <li><span>Image 05</span><div><h3>BLOCK·CHAIN</h3></div></li>
+        <li><span>Image 06</span><div><h3>WIS·DOM</h3></div></li>
     </ul>
 
     <div class="superstar">
