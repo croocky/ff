e4c14602924b8db71f2292766d521dda1c78bdcb
cruelka <kes1993@ya.ru>
2018-01-09 23:13:48 +0400

    htaccess

--- blog/app/Http/Controllers/UserInside.php
+++ blog/app/Http/Controllers/UserInside.php
@@ -36,7 +36,7 @@ class UserInside extends Controller
             $deposit = $currentuser->deposit;
             $level = Levels::where('pers_amount','<',$deposit)->orderBy('pers_amount', 'desc')->take(1)->get();
             $refferals = array_filter(explode(',',$currentuser->structure));
-            $first_level = User::whereIn('id',$refferals)->pluck('id',['name','deposit']);
+            $first_level = User::whereIn('id',$refferals)->pluck('id','name','deposit');
             var_dump($first_level);
 
             foreach($refferals as $refferal){
