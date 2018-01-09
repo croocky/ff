5ed34ab624add783b80499045638c28b27a3824b
cruelka <kes1993@ya.ru>
2018-01-09 22:48:50 +0400

    htaccess

--- blog/app/Http/Controllers/UserInside.php
+++ blog/app/Http/Controllers/UserInside.php
@@ -35,8 +35,7 @@ class UserInside extends Controller
             }
             $deposit = $currentuser->deposit;
             $level = Levels::where('pers_amount','<',$deposit)->orderBy('pers_amount', 'desc')->take(1)->get();
-            //$referrals = explode
-
+            var_dump($currentuser);
             return view('lc', ['user' => $currentuser, 'level' => $level[0]]);
         }
         else{
