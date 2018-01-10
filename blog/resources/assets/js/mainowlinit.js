6b5fdabf2ac83d567f25ca126e474472a4b1164c
socaseinpoint <socaseinpoint@gmail.com>
2018-01-10 20:51:56 +0400

    app js

--- blog/resources/assets/js/mainowlinit.js
+++ blog/resources/assets/js/mainowlinit.js
@@ -2,19 +2,22 @@ require('jquery');
 
 require ('owl.carousel');
 
-$('.cryptostats').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
-})
+$(document).ready(function(){
+  $('.cryptostats').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
+
+});
