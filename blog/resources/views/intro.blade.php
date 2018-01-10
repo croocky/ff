8bc5c148b84f0ff407cbff792053eb3716f04309
socaseinpoint <socaseinpoint@gmail.com>
2018-01-10 20:44:50 +0400

    pack

--- blog/resources/views/intro.blade.php
+++ blog/resources/views/intro.blade.php
@@ -220,25 +220,6 @@
 
   </div>
 
-  <script type="text/javascript">
-    $('.cryptostats').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:3
-          },
-          1000:{
-              items:5
-          }
-      }
-    })
-  </script>
-
 
 
 
