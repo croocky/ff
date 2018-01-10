30c563fa8b77c642fb9804fee889985cc86bbe0d
socaseinpoint <socaseinpoint@gmail.com>
2018-01-10 20:20:51 +0400

    app

--- blog/resources/views/intro.blade.php
+++ blog/resources/views/intro.blade.php
@@ -24,7 +24,8 @@
 
     <div class="button-registration"><a href="/login" class="button-registration__inner">Registration</div></a>
 
-    <div class="sector bg_black">
+    <div class=" bg_black">
+      <div class="sector">
       <div class="heading text_white">ADVANTAGES</div>
       <div class="row text-center">
         <div class="four columns">
@@ -44,7 +45,7 @@
             You can use the currency anywhere in the worldе</div>
         </div>
       </div>
-
+    </div>
     </div>
 
 
@@ -73,20 +74,22 @@
         </div>
     </div>
 
-    <div class="sector bg_black">
-      <div class="heading heading_white">Cryptocurrencies</div>
-      <div class="row cryptostats">
-        <div class="four columns">
-          <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/btc.svg') }}" alt="BTC"> BTC = 14550$</p>
-          <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/1.svg') }}" alt="BTC GRAPH">
-        </div>
-        <div class="four columns">
-          <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/eth.svg') }}" alt="ETH"> ETH = 1228$</p>
-          <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/2.svg') }}" alt="ETH GRAPH">
-        </div>
-        <div class="four columns">
-          <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/ltc.svg') }}" alt="LTC"> LTC = 250$</p>
-          <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/3.svg') }}" alt="LTC GRAPH">
+    <div class="bg_black">
+      <div class="sector">
+        <div class="heading heading_white">Cryptocurrencies</div>
+        <div class="row cryptostats">
+          <div class="four columns">
+            <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/btc.svg') }}" alt="BTC"> BTC = 14550$</p>
+            <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/1.svg') }}" alt="BTC GRAPH">
+          </div>
+          <div class="four columns">
+            <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/eth.svg') }}" alt="ETH"> ETH = 1228$</p>
+            <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/2.svg') }}" alt="ETH GRAPH">
+          </div>
+          <div class="four columns">
+            <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/ltc.svg') }}" alt="LTC"> LTC = 250$</p>
+            <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/3.svg') }}" alt="LTC GRAPH">
+          </div>
         </div>
       </div>
 
