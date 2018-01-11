9735868d1669f7f55cc25005f2d5f9ae606e5434
socaseinpoint <socaseinpoint@gmail.com>
2018-01-11 16:25:08 +0400

    intro

--- blog/resources/views/intro.blade.php
+++ blog/resources/views/intro.blade.php
@@ -80,8 +80,8 @@
         <div class="cryptostats owl-carousel">
           <div class="">
             <div class="columns six">
-            <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/eth.svg') }}" alt="ETH"> ETH = 1228$</p>
-            <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/2.svg') }}" alt="ETH GRAPH">
+            <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/btc.svg') }}" alt="ETH"> BTC = 13700$</p>
+            <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/1.svg') }}" alt="ETH GRAPH">
             </div>
             <div class="columns six">
               <div class="">
@@ -113,7 +113,7 @@
           </div>
           <div class="">
             <div class="columns six">
-              <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/ltc.svg') }}" alt="LTC"> LTC = 250$</p>
+              <p class="cryptostats__heading">1 <img src="{{ asset('imgsvg/cc_white/ltc.svg') }}" alt="LTC"> LTC = 1203</p>
               <img class="cryptostats__img" src="{{ asset('imgsvg/cryptostats/3.svg') }}" alt="LTC GRAPH">
             </div>
             <div class="columns six">
